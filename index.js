69d85ded74e132677d38e07474f6d441acdbea5c
Rezoan Ahmed <rezoan.cse.2017@gmail.com>
2023-12-23 01:27:56 +0600

    task post and get method has been added

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
 require('dotenv').config();
 
+// middlewares
+app.use(cors())
+app.use(express.json());
 // mongodb
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -22,6 +26,22 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
+
+    // database collection
+    const todosCollection = client.db("Todos").collection("myTodos");
+
+    app.post("/todos", async(req,res)=>{
+      const todo = req.body;
+      const result = await todosCollection.insertOne(todo);
+      res.send(result);
+    })
+    app.get("/todos", async(req,res)=>{
+      const result = await todosCollection.find().toArray();
+      res.send(result);
+    })
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
